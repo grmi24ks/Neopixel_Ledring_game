0bac888e90464c244ab484c6b24d0aa6057cf3ee
Mikael Grafström <mikael_grafstrom@hotmail.com>
2026-08-03 11:47:32 +0200

    Complete game logic with state machine and restart

--- src/main.cpp
+++ src/main.cpp
@@ -2,29 +2,36 @@
 
 //DEFINITIONS AND DATATYPES
 
-unsigned long animation_start_time = 0;
-const unsigned long hit_duration = 700;
-unsigned long animation_previous_time = 0;
-int animation_blink_count = 0;
-bool animation_leds_on = false;
+unsigned long animation_start_time = 0;                   //används för att beräkna när LED:s ska släckas vid träffar
+const unsigned long hit_duration = 700;                   //dito
+unsigned long animation_previous_time = 0;                //används för att beräkna när blink:s ska sluta vid missar och vinst
+int animation_blink_count = 0;                            //räknar antal blink
+bool animation_leds_on = false;                           //värdet som jag använder för att växla med: animation_leds_on/!animation_leds_on
 
-unsigned long previous_time = 0;
-unsigned long led_interval = 250;
-unsigned long last_debounce_time = 0;
-const unsigned long debounce_interval = 30;
 
-int stable_button_state = HIGH;
-int previous_button_reading = HIGH; //knapp inte nedtryckt
+const unsigned long start_led_interval = 250;
+const int start_lives = 5;
+const int start_position = 0;
+
+
+unsigned long previous_time = 0;                          //huvudvariabeln för att beräkna när en viss tid gått, ex.vis för att avgöra när LED:ljuset ska "vandra"
+unsigned long led_interval = 250;                         //den tid varje LED ska lysa när den vandrar i spelet
+unsigned long last_debounce_time = 0;                     //används för att beräkna tiden sedan den råa knappavläsningen senast ändrades
+const unsigned long debounce_interval = 30;               //knappvärdet måste vara oförändrat i minst 30 ms samt vara != stable_button_state innan det godkänns
+
+int stable_button_state = HIGH;                           //utgångsläge är knapp ej nedtryckt = HIGH
+int previous_button_reading = HIGH;                       //knapp inte nedtryckt
+bool restart_armed = false;                               //restart "laddad" men körs först när knapptryckning godkänts (oförändr. >30ms och != stable_button_state)
 const int buttonPin = A0;
 
 const int ledPins[] = {D2, D3, D4, D5, D6, D7, D8, D9, D10, D11};
 const int ledCount = 10;
-int current_position = 0;
-int target_position = 7;
-int direction = 1;     // 1 betyder rörelse vä->hö, -1 tvärtom
+int current_position = 0;                                 //index för den LED som är tänd; första elementet i arrayen har index 0
+int target_position = 4;
+int direction = 1;                                        //1 betyder rörelse vä->hö, -1 tvärtom
 
 int score = 0;
-int lives = 10;
+int lives = 5;
 
 enum GameState //gammal lösning kunde ge running = 1, game_won = 1, game_lost = 1. Här går det inte - spelet kan bara vara i ett läge åt gången
 {
@@ -40,13 +47,15 @@ GameState game_state = PLAYING; // skapar en enum-variabel och sätter grundläg
 
 static void handle_hit(void);
 static void handle_miss(void);
-static void show_game_lost(void);
+static void show_game_lost(void);                                 //behöver ingen animation eftersom vi bara släcker ned direkt
 static void start_hit_animation(unsigned long current_time);
 static void update_hit_animation(unsigned long current_time);
 static void start_miss_animation(unsigned long current_time);
 static void update_miss_animation(unsigned long current_time);
 static void start_won_animation(unsigned long current_time);
 static void update_won_animation(unsigned long current_time);
+static void reset_game(unsigned long current_time);
+
 
 
 //FUNCTIONS
@@ -55,7 +64,7 @@ void setup(void)
 {
   Serial.begin(9600);
   
-  pinMode(buttonPin, INPUT_PULLUP);  //sätter A0 som ingång och med internt motstånd, sk PULLUP
+  pinMode(buttonPin, INPUT_PULLUP);  //sätter A0 som ingång och med internt motstånd, sk PULLUP vilket knappen behöver 
   
   for (int i = 0; i < ledCount; i++) //smidigt sätt att assign:a LED-lamporna till fysiska OUTPUTS och sätta dem till släckta == LOW 
     {
@@ -69,6 +78,8 @@ void setup(void)
 
 static void handle_hit(void)
 {
+  int previous_target_position = target_position;
+  
   score = score + 100;
   
   Serial.print("Score: ");
@@ -76,8 +87,19 @@ static void handle_hit(void)
   Serial.print("Lives remaining: ");
   Serial.println(lives);
 
-  led_interval = led_interval - 50;
+  do 
+  {
+    target_position = random(ledCount);
+  }  
+  while (target_position == previous_target_position);
 
+  previous_target_position = target_position;
+  
+  Serial.print("New target: ");
+  Serial.println(target_position);
+  
+  led_interval = led_interval - 50;                         //LED-vandringen blir snabbare och snabbare för varje HIT
+  
   if (led_interval == 0)
   {
     Serial.println("CONGRATULATIONS - YOU WON THE GAME!");
@@ -101,7 +123,7 @@ static void handle_miss(void)
 
 static void show_game_lost(void)
 {
-  for (int i = 0; i < ledCount; i++)       //öka LED nr så att nästa LED-lampa kan tändas
+  for (int i = 0; i < ledCount; i++)                        //gå igenom och släck alla LED-lampor
   {
     digitalWrite(ledPins[i], LOW);
   }
@@ -112,25 +134,25 @@ static void start_hit_animation(unsigned long current_time)
 
 {
   for (int i = 0; i < ledCount; i++)
-  {
-    digitalWrite(ledPins[i], HIGH);
+  { 
+    digitalWrite(ledPins[i], HIGH);                          //tänd alla LED:S
     
   }
   
-  animation_start_time = current_time;
+  animation_start_time = current_time;                       //uppdatera animation_start_time för att nedan kunna beräkna och jämföra med hit_duration
   game_state = SHOWING_HIT;
 }
 
 static void update_hit_animation(unsigned long current_time)
 {
-  if (current_time - animation_start_time >= hit_duration)
+  if (current_time - animation_start_time >= hit_duration)    //om 700 ms har passerat...
   {
     for (int i = 0; i < ledCount; i++)
     {
-      digitalWrite(ledPins[i], LOW);
+      digitalWrite(ledPins[i], LOW);                          //släck alla LED:s
     }
-    digitalWrite(ledPins[current_position], HIGH);  //tänd den aktuella LED-lampan
-    game_state = PLAYING;
+    digitalWrite(ledPins[current_position], HIGH);            //tänd den aktuella LED-lampan 
+    game_state = PLAYING;                                     //återgå till huvudlooop:en med dess LED-vandring och knappavläsning
   }  
 }
 
@@ -145,15 +167,15 @@ static void start_miss_animation(unsigned long current_time)
       digitalWrite(ledPins[i], HIGH);
     }
   
-  game_state = SHOWING_MISS;
+  game_state = SHOWING_MISS;                                        //detta leder oss via huvudloopen till update_miss_animation nedan
 }
 
 static void update_miss_animation(unsigned long current_time)
 {
-  if (current_time - animation_previous_time >= 100)                //om 100 ms har gått... (vi har en separat timer för animationen)
+  if (current_time - animation_previous_time >= 75)                //om 75 ms har gått... (vi har en separat timer för MISS-animationen)
   {
     animation_previous_time = current_time;                         //spara tiden för den senaste växlingen i MISS-animationen
-    animation_leds_on = !animation_leds_on;                              //du måsta byta till motsatsen av dess nuvarande värde - annars sker ingen växling!!
+    animation_leds_on = !animation_leds_on;                         //du måste byta till motsatsen av dess nuvarande värde - annars sker ingen växling!!
 
     for (int i = 0; i < ledCount; i++)                         
     {
@@ -162,13 +184,13 @@ static void update_miss_animation(unsigned long current_time)
     
     if (animation_leds_on == false)                                 //om lamporna nu är släckta
     {
-      animation_blink_count = animation_blink_count + 1;                 //öka blinkräknaren
+      animation_blink_count = animation_blink_count + 1;            //öka blinkräknaren
     }   
     if (animation_blink_count >= 3)                                 //tre blinkningar är färdiga
     {
       digitalWrite(ledPins[current_position], HIGH);            //tänd current position igen och byt tillbaka till PLAYING
-      previous_time = current_time;                             //synkningen gör att den återställda LED:n får lysa ett helt led_interval innan den flyttar sig igen, annars kanske ett hopp sker
-      game_state = PLAYING;                                     //eftersom previous_time fortfarande innehöll tiden från före animationen (som pågår ganska länge)
+      previous_time = current_time;                             //återställ huvud-timer:n så att den aktuella LED-lampan får lysa ett helt led_interval efter miss-animationen.
+      game_state = PLAYING;                                     // 
     }
   }
 }
@@ -190,7 +212,7 @@ static void start_won_animation(unsigned long current_time)
 
 static void update_won_animation(unsigned long current_time)
 {
- if (current_time - animation_previous_time >= 100)                //om 100 ms har gått... (vi har en separat timer för animationen)
+ if (current_time - animation_previous_time >= 100)                 //om 100 ms har gått... (vi har en separat timer för animationen)
   {
     animation_previous_time = current_time;                         //spara tiden för den senaste växlingen i animationen
     animation_leds_on = !animation_leds_on;                         //du måsta byta till motsatsen av dess nuvarande värde - annars sker ingen växling!!
@@ -202,9 +224,9 @@ static void update_won_animation(unsigned long current_time)
     
     if (animation_leds_on == false)                                 //om lamporna nu är släckta
     {
-      animation_blink_count = animation_blink_count + 1;                 //öka blinkräknaren
+      animation_blink_count = animation_blink_count + 1;            //öka blinkräknaren
     }   
-    if (animation_blink_count >= 20)                                 //om 20 blinkningar är räknade
+    if (animation_blink_count >= 20)                                //om 20 blinkningar/släckningar/varv är räknade
     {
       for (int i = 0; i < ledCount; i++)                         
       {
@@ -215,24 +237,76 @@ static void update_won_animation(unsigned long current_time)
   }
 }
 
+static void reset_game(unsigned long current_time)
+{
+  led_interval = start_led_interval;
+  lives = start_lives;
+  score = 0;
+  current_position = start_position;
+  direction = 1;
+  previous_time = current_time;
+
+  for (int i = 0; i < ledCount; i++)
+  {
+    digitalWrite(ledPins[i], LOW);
+  }
+  
+  digitalWrite(ledPins[current_position], HIGH);
+
+  game_state = PLAYING;
+}
+
+//HUVUDLOOP
 
 void loop(void)
 {
-  unsigned long current_time = millis();          //läs aktuell tid från mcu-start med funktionen millis. Max typ 49 dygn. Måste ligga före de två kontrollerna nedan
+  //måste ligga före de 5 kontrollerna nedan    
+  unsigned long current_time = millis();                      //läs aktuell tid från mcu-start med funktionen millis. Slår om efter typ 49 dygn, tidsdiff fungerar eftersom unsigned long används.  
 
-  if(game_state ==  SHOWING_HIT)                  //har 700 ms gått? har 700 ms gått? 
+  if (game_state == WON || game_state == LOST)
+  {
+    int button_state = digitalRead(buttonPin);                 //rå knappavläsning
+    
+    if (button_state != previous_button_reading)                   //KNAPP: om råvärdet skiljer sig från förra gången, till exempel användaren släppte upp knappen efter WON/LOST
+    {
+      last_debounce_time = current_time;                       //starta om debounce-timern
+    }  
+    
+    if (current_time - last_debounce_time >= debounce_interval) //om längre tid än 30 ms gått...så är knappförändringen stabil
+    {
+      if (button_state != stable_button_state)
+      {
+        stable_button_state = button_state;
+
+        if (stable_button_state == HIGH)                         //om stabil avläsningen är HIGH - ej nedtryckt, armeras omstart
+        {
+        restart_armed = true;
+        }
+        
+        else if (stable_button_state == LOW && restart_armed)
+        {
+          reset_game(current_time);
+          restart_armed = false;
+        }
+      }
+    }
+    previous_button_reading = button_state;                     //annars kan nästa loopvarv inte upptäcka en ny rå förändring korrekt
+    return;
+  }
+ 
+  if (game_state ==  SHOWING_HIT)                  //kör träffanimationen (som frågar har 700 ms gått) och hoppa över resten av huvudloop:en.
   {
     update_hit_animation(current_time);
     return;
   }
   
-  if(game_state == SHOWING_MISS)
+  if (game_state == SHOWING_MISS)
   {
     update_miss_animation(current_time);
     return;
   }
 
-  if(game_state == SHOWING_WON)
+  if (game_state == SHOWING_WON)
   {
     update_won_animation(current_time);
     return;
@@ -240,85 +314,85 @@ void loop(void)
   
   if (game_state != PLAYING)
   {
-    return;                                            //detta avslutar det aktuella loopvarvet direkt. Programmet fortsätter, ser denna state och upplevs avslutad av användaren
+    return;                                            //detta avslutar det aktuella loopvarvet direkt. Programmet fortsätter, ser denna state varje loop-varv och upplevs avslutad av användaren
   }
 
 
 
-    int button_state = digitalRead(buttonPin);        //KNAPP: läs och skriv in råa värdet på knappnedtryckning (HIGH - ej nedtryckt eller LOW - neddtryckt till variabel button_state)
-    
-    
+  int button_state = digitalRead(buttonPin);        //KNAPP: läs och skriv in råa värdet på knappnedtryckning (HIGH - ej nedtryckt eller LOW - nedtryckt till variabel button_state)
+  
+  
 
-    if (current_time - previous_time >= led_interval) //OM angiven tid för den tid en LED ska lysa har passerat...
+  if (current_time - previous_time >= led_interval) //OM angiven tid för den tid en LED ska lysa har passerat...
+  {
+    previous_time = current_time;                   //uppdatera tid för senaste tändning/släckning
+    
+    digitalWrite(ledPins[current_position], LOW);   //släck current_position-LED     
+    
+    current_position = current_position + direction;
+    
+    if (current_position >= ledCount)               // Om positionen passerar sista LED:n, flytta tillbaka ett steg och byt riktning
     {
-      previous_time = current_time;                   //uppdatera tid för senaste tändning/släckning
-      
-      digitalWrite(ledPins[current_position], LOW);   //släck current_position-LED     
-      
-      current_position = current_position + direction;
-      
-      if (current_position >= ledCount)               // Om positionen passerar sista LED:n, flytta tillbaka ett steg och byt riktning
-      {
-        current_position = ledCount - 2;
-        direction = -1;
-      }
-      else if (current_position < 0)                  // Om positionen passerar första LED:n, flytta fram ett steg och byt riktning
-      {
-        current_position = 1;
-        direction = 1;
-      }
-      digitalWrite(ledPins[current_position], HIGH);  //tänd den aktuella LED-lampan//öka LED nr så att nästa LED-lampa kan styras
+      current_position = ledCount - 2;
+      direction = -1;
     }
-    
+    else if (current_position < 0)                  // Om positionen passerar första LED:n, flytta fram ett steg och byt riktning
+    {
+      current_position = 1;
+      direction = 1;
+    }
+    digitalWrite(ledPins[current_position], HIGH);  //tänd LED-lampan på den nya positionen
+  }
+  
 
-    if (button_state != previous_button_reading)      //KNAPP: om knappförändring skett sedan förra loopvarvet
+  if (button_state != previous_button_reading)      //KNAPP: om knappförändring skett sedan förra loopvarvet
+  {
+    last_debounce_time = current_time;
+  } 
+  
+  if (current_time - last_debounce_time >= debounce_interval) //om längre tid än 30 ms gått...
+  {
+    if (button_state != stable_button_state)                   //om vi dessutom ser ett knappskifte - konstatera att det ägde rum och...
     {
-      last_debounce_time = current_time;
-    } 
-    
-    if (current_time - last_debounce_time >= debounce_interval) //om längre tid än 30 ms gått...
-    {
-      if (button_state != stable_button_state)                   //om vi dessutom ser ett knappskifte - konstatera att det ägde rum och...
+      stable_button_state = button_state;                      //...uppdatera knappläget
+      if (stable_button_state == LOW)                          //om det nya knappläget är nedtryckt = LOW
       {
-        stable_button_state = button_state;                      //...uppdatera knappläget
-        if (stable_button_state == LOW)                          //om det nya knappläget är nedtryckt = LOW
+        if (current_position == target_position)             //OCH om knappen tryckts ned då den aktuella lampan motsvarar mål-lampan
         {
-            if (current_position == target_position)             //OCH om knappen tryckts ned då den aktuella lampan motsvarar mål-lampan
-            {
-              Serial.println("HIT!");                            //HIT!
-              handle_hit();
-              
-              if (game_state == WON)
-              {
-                start_won_animation(current_time);
-              }
-              else
-              {
-              start_hit_animation(current_time);                
-              }
-              
-            }
-            else
-            {
-              Serial.println("MISS!");
-              handle_miss();
-              
-              if (game_state == LOST)
-              {
-                show_game_lost();
-              }
-              else
-              {
-                start_miss_animation(current_time);
-              }
-              
-              
-             
-            }
+          Serial.println("HIT!");                            //HIT!
+          handle_hit();
+          
+          if (game_state == WON)
+          {
+            start_won_animation(current_time);
+          }
+          else
+          {
+          start_hit_animation(current_time);                
+          }
+          
+        }
+        else
+        {
+          Serial.println("MISS!");
+          handle_miss();
+          
+          if (game_state == LOST)
+          {
+            show_game_lost();
+          }
+          else
+          {
+            start_miss_animation(current_time);
+          }
+          
+          
+          
         }
       }
     }
-    
-    previous_button_reading = button_state;       //uppdatera senaste knappavläsning
+  }
+  
+  previous_button_reading = button_state;       //uppdatera senaste knappavläsning
   
 }
