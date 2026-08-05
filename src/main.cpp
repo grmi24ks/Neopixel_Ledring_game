0cdfb8b99c76a8c72bc1c47cc246c933cb10d187
Mikael Grafström <mikael_grafstrom@hotmail.com>
2026-08-05 08:31:12 +0200

    Initial game version

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,8 @@
 #include <Arduino.h>
+#include <Adafruit_NeoPixel.h>
+
+#define DATA_PIN 5
+#define PIXEL_COUNT 24
 
 //DEFINITIONS AND DATATYPES
 
@@ -24,14 +28,19 @@ int previous_button_reading = HIGH;                       //knapp inte nedtryckt
 bool restart_armed = false;                               //restart "laddad" men körs först när knapptryckning godkänts (oförändr. >30ms och != stable_button_state)
 const int buttonPin = A0;
 
-const int ledPins[] = {D2, D3, D4, D5, D6, D7, D8, D9, D10, D11};
-const int ledCount = 10;
 int current_position = 0;                                 //index för den LED som är tänd; första elementet i arrayen har index 0
-int target_position = 4;
+int target_position = 2;
 int direction = 1;                                        //1 betyder rörelse vä->hö, -1 tvärtom
 
 int score = 0;
 int lives = 5;
+
+Adafruit_NeoPixel ring(
+    PIXEL_COUNT,
+    DATA_PIN,
+    NEO_GRB + NEO_KHZ800
+);
+
 
 enum GameState //gammal lösning kunde ge running = 1, game_won = 1, game_lost = 1. Här går det inte - spelet kan bara vara i ett läge åt gången
 {
@@ -66,19 +75,14 @@ void setup(void)
   
   pinMode(buttonPin, INPUT_PULLUP);  //sätter A0 som ingång och med internt motstånd, sk PULLUP vilket knappen behöver 
   
-  for (int i = 0; i < ledCount; i++) //smidigt sätt att assign:a LED-lamporna till fysiska OUTPUTS och sätta dem till släckta == LOW 
-    {
-        pinMode(ledPins[i], OUTPUT);
-        digitalWrite(ledPins[i], LOW);
-    }
-
-  digitalWrite(ledPins[current_position], HIGH);  //tänd den aktuella LED-lampan
-
+  ring.begin();
+  ring.clear();
+  ring.show();
 }
 
 static void handle_hit(void)
 {
-  int previous_target_position = target_position;
+  /*int previous_target_position = target_position;*/
   
   score = score + 100;
   
@@ -86,15 +90,19 @@ static void handle_hit(void)
   Serial.println(score);
   Serial.print("Lives remaining: ");
   Serial.println(lives);
-
-  led_interval = led_interval - 50;                         //LED-vandringen blir snabbare och snabbare för varje HIT
   
-  if (led_interval == 0)
+  if (led_interval <= 50)
     {
+      led_interval = 0;
       Serial.println("CONGRATULATIONS - YOU WON THE GAME!");
       game_state = WON;
     }
-  else
+    else
+    {
+      led_interval -=50;                    //LED-vandringen blir snabbare och snabbare för varje HIT
+
+    }
+  /*else
   {
     do 
     {
@@ -102,9 +110,10 @@ static void handle_hit(void)
     }  
     while (target_position == previous_target_position);
 
+
     Serial.print("New target: ");
     Serial.println(target_position);
-  }
+  }*/
   
 }
 
@@ -123,37 +132,40 @@ static void handle_miss(void)
 
 
 static void show_game_lost(void)
-{
-  for (int i = 0; i < ledCount; i++)                        //gå igenom och släck alla LED-lampor
-  {
-    digitalWrite(ledPins[i], LOW);
-  }
+{                   
+ring.clear();           //gå igenom och släck alla LED-lampor
+ring.show();
 }
-
 
 static void start_hit_animation(unsigned long current_time)
 
 {
-  for (int i = 0; i < ledCount; i++)
+  for (int i = 0; i < PIXEL_COUNT; i++)
   { 
-    digitalWrite(ledPins[i], HIGH);                          //tänd alla LED:S
-    
+    ring.setPixelColor(                                         //buffra pixlar, färg och ljusstyrka (här för samtliga pixlar)
+        i, 
+        ring.Color(0, 25, 0)
+    );
   }
-  
-  animation_start_time = current_time;                       //uppdatera animation_start_time för att nedan kunna beräkna och jämföra med hit_duration
+  ring.show();                                                 //visa vad som ligger i bufferten
+
+  animation_start_time = current_time;                         //uppdatera animation_start_time för att nedan kunna beräkna och jämföra med hit_duration
   game_state = SHOWING_HIT;
 }
 
 static void update_hit_animation(unsigned long current_time)
 {
-  if (current_time - animation_start_time >= hit_duration)    //om 700 ms har passerat...
+  if (current_time - animation_start_time >= hit_duration)      //om 700 ms har passerat...
   {
-    for (int i = 0; i < ledCount; i++)
-    {
-      digitalWrite(ledPins[i], LOW);                          //släck alla LED:s
-    }
-    digitalWrite(ledPins[current_position], HIGH);            //tänd den aktuella LED-lampan 
-    game_state = PLAYING;                                     //återgå till huvudlooop:en med dess LED-vandring och knappavläsning
+    ring.clear();                                             //töm bufferten - här innebär det släck alla LED:s
+                                                
+    ring.setPixelColor(                                         //buffra pixlar, färg och ljusstyrka (OBSERVERA: här för current_position)
+        current_position, 
+        ring.Color(0, 0, 25)
+    );
+    ring.show();                                                 //visa vad som ligger i bufferten
+    
+    game_state = PLAYING;                                        //återgå till huvudlooop:en med dess LED-vandring och knappavläsning
   }  
 }
 
@@ -163,11 +175,17 @@ static void start_miss_animation(unsigned long current_time)
   animation_leds_on = true;
   animation_previous_time = current_time;
   
-  for (int i = 0; i < ledCount; i++)
-    {
-      digitalWrite(ledPins[i], HIGH);
-    }
+  for (int i = 0; i < PIXEL_COUNT; i++)
+  {
+    ring.setPixelColor(                                          
+      i, 
+      ring.Color(25, 0, 0)
+    );
+                                                  
+  }
   
+  ring.show();    
+
   game_state = SHOWING_MISS;                                        //detta leder oss via huvudloopen till update_miss_animation nedan
 }
 
@@ -178,10 +196,20 @@ static void update_miss_animation(unsigned long current_time)
     animation_previous_time = current_time;                         //spara tiden för den senaste växlingen i MISS-animationen
     animation_leds_on = !animation_leds_on;                         //du måste byta till motsatsen av dess nuvarande värde - annars sker ingen växling!!
 
-    for (int i = 0; i < ledCount; i++)                         
+    ring.clear();
+
+    if (animation_leds_on == true)
     {
-      digitalWrite(ledPins[i], animation_leds_on);                  //skriva det nya läget till alla LED
-    }
+      for (int i = 0; i < PIXEL_COUNT; i++)                         
+      {
+        ring.setPixelColor(                                         
+          i, 
+          ring.Color(25, 0, 0)
+        );                
+      }
+    }                             
+    
+    ring.show();                                                    //tänd alla lampor
     
     if (animation_leds_on == false)                                 //om lamporna nu är släckta
     {
@@ -189,9 +217,16 @@ static void update_miss_animation(unsigned long current_time)
     }   
     if (animation_blink_count >= 3)                                 //tre blinkningar är färdiga
     {
-      digitalWrite(ledPins[current_position], HIGH);            //tänd current position igen och byt tillbaka till PLAYING
-      previous_time = current_time;                             //återställ huvud-timer:n så att den aktuella LED-lampan får lysa ett helt led_interval efter miss-animationen.
-      game_state = PLAYING;                                     // 
+      ring.clear();
+      
+      ring.setPixelColor(                                           
+          current_position, 
+          ring.Color(0, 0, 25)
+      );
+      ring.show();                                                 
+                                                        
+      previous_time = current_time;                                 //återställ huvud-timer:n så att den aktuella LED-lampan får lysa ett helt led_interval efter miss-animationen.
+      game_state = PLAYING;                                     
     }
   }
 }
@@ -203,37 +238,57 @@ static void start_won_animation(unsigned long current_time)
   animation_leds_on = true;
   animation_previous_time = current_time;
   
-  for (int i = 0; i < ledCount; i++)
-    {
-      digitalWrite(ledPins[i], HIGH);
-    }
-  
+  for (int i = 0; i < PIXEL_COUNT; i++)                         
+  {
+    ring.setPixelColor(                                           
+        i, 
+        ring.Color(0, 25, 0)
+    );                
+  }
+
+  ring.show();
+    
   game_state = SHOWING_WON;
 }
 
 static void update_won_animation(unsigned long current_time)
 {
- if (current_time - animation_previous_time >= 100)                 //om 100 ms har gått... (vi har en separat timer för animationen)
+ if (current_time - animation_previous_time >= 75)                //om 75 ms har gått... (vi har en separat timer för MISS-animationen)
   {
-    animation_previous_time = current_time;                         //spara tiden för den senaste växlingen i animationen
-    animation_leds_on = !animation_leds_on;                         //du måsta byta till motsatsen av dess nuvarande värde - annars sker ingen växling!!
+    animation_previous_time = current_time;                         //spara tiden för den senaste växlingen i MISS-animationen
+    animation_leds_on = !animation_leds_on;                         //du måste byta till motsatsen av dess nuvarande värde - annars sker ingen växling!!
 
-    for (int i = 0; i < ledCount; i++)                         
+    ring.clear();
+
+    if (animation_leds_on == true)
     {
-      digitalWrite(ledPins[i], animation_leds_on);                  //skriva det nya läget till alla LED
-    }
+      for (int i = 0; i < PIXEL_COUNT; i++)                         
+      {
+        ring.setPixelColor(                                         
+            i, 
+            ring.Color(0, 25, 0)
+        );                
+      }
+    }                             
+    
+    ring.show();                                                    
     
     if (animation_leds_on == false)                                 //om lamporna nu är släckta
     {
       animation_blink_count = animation_blink_count + 1;            //öka blinkräknaren
     }   
-    if (animation_blink_count >= 20)                                //om 20 blinkningar/släckningar/varv är räknade
+    if (animation_blink_count >= 20)                                 //20 blinkningar är färdiga
     {
-      for (int i = 0; i < ledCount; i++)                         
-      {
-        digitalWrite(ledPins[i], LOW);                  
-      }  
-      game_state = WON;                               
+      ring.clear();
+      
+      ring.setPixelColor(                                           
+          current_position, 
+          ring.Color(0, 0, 25)
+      );
+      ring.show();                                                 
+                                                        
+      previous_time = current_time;                                 //återställ huvud-timer:n så att den aktuella LED-lampan får lysa ett helt led_interval efter miss-animationen.
+      game_state = WON;                                     
     }
   }
 }
@@ -247,12 +302,14 @@ static void reset_game(unsigned long current_time)
   direction = 1;
   previous_time = current_time;
 
-  for (int i = 0; i < ledCount; i++)
-  {
-    digitalWrite(ledPins[i], LOW);
-  }
+  ring.clear();
+
+  ring.setPixelColor(                                          
+      current_position, 
+      ring.Color(0, 0, 25)
+  );
   
-  digitalWrite(ledPins[current_position], HIGH);
+  ring.show();                                                 
 
   game_state = PLAYING;
 }
@@ -327,22 +384,28 @@ void loop(void)
   if (current_time - previous_time >= led_interval) //OM angiven tid för den tid en LED ska lysa har passerat...
   {
     previous_time = current_time;                   //uppdatera tid för senaste tändning/släckning
-    
-    digitalWrite(ledPins[current_position], LOW);   //släck current_position-LED     
-    
+        
+    ring.clear();                                   //släck LED   
+
     current_position = current_position + direction;
     
-    if (current_position >= ledCount)               // Om positionen passerar sista LED:n, flytta tillbaka ett steg och byt riktning
+    if (current_position >= PIXEL_COUNT)               // Om positionen passerar sista LED:n, flytta tillbaka ett steg och byt riktning
     {
-      current_position = ledCount - 2;
-      direction = -1;
+    current_position = PIXEL_COUNT - 2;
+    direction = -1;
     }
     else if (current_position < 0)                  // Om positionen passerar första LED:n, flytta fram ett steg och byt riktning
     {
-      current_position = 1;
-      direction = 1;
+    current_position = 1;
+    direction = 1;
     }
-    digitalWrite(ledPins[current_position], HIGH);  //tänd LED-lampan på den nya positionen
+
+    ring.setPixelColor(                           
+        current_position, 
+        ring.Color(0, 0, 25)
+    );  
+
+    ring.show();
   }
   
 
